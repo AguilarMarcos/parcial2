7eb5fb7a7766581ac4084c22343a1818683db6c3
AguilarMarcos <marcosuriel331@gmail.com>
2026-03-05 08:53:12 -0600

    cambio

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Proyecto from './Proyecto';
-import proyecto from './Proyecto';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
